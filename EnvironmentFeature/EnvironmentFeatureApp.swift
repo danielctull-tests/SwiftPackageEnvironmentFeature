276752a8708cce62a12ea42f21cf799aec29c938
Daniel Tull <dt@danieltull.co.uk>
2023-07-20 15:44:57 +0100

    Initial Commit

---
+++ EnvironmentFeature/EnvironmentFeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentFeatureApp.swift
+//  EnvironmentFeature
+//
+//  Created by Tull, Daniel on 20/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentFeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
